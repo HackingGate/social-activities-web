46f6bb5f92a0896c3b095ea60315184d56b677b1
Tong <8541644+HackingGate@users.noreply.github.com>
2022-07-01 21:21:51 +0900

    Refactor Layout (#8)

---
+++ pages/components/header.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import styles from '../../styles/Header.module.css'
+
+const Header: NextPage = () => (
+  <header className={styles.header}>
+    <h1>Header</h1>
+  </header>
+)
+
+export default Header
---
+++ pages/components/layout.tsx
@@ -0,0 +1,21 @@
+import styles from '../../styles/Layout.module.css'
+import { NextPage } from 'next'
+import { ReactElement } from 'react'
+import Footer from './footer'
+import Header from './header'
+
+interface LayoutProps {
+  children: ReactElement
+}
+
+const Layout: NextPage<LayoutProps> = (props) => (
+  <div className={styles.container}>
+    <Header />
+
+    <main className={styles.main}>{props.children}</main>
+
+    <Footer />
+  </div>
+)
+
+export default Layout
