1250ebde806312dae06869e86f51862fd8103232
HackingGate <8541644+HackingGate@users.noreply.github.com>
2022-07-28 22:47:07 +0900

    Remove custom configuration

--- src/components/accounts/Session.tsx
+++ src/components/accounts/Session.tsx
@@ -3,11 +3,9 @@ import { useSession, signIn, signOut } from 'next-auth/react'
 export default function Session() {
   const { data: session } = useSession()
   if (session) {
-    const { accessToken } = session
     return (
       <>
         Signed in as {session.user.email} <br />
-        Access Token: {accessToken} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     )
